9ea5d68b8e1d80ddeac7642ca77e9064a5c91cd7
nikitasmall <nikitasosnov@yahoo.com>
2015-04-22 23:17:53 +0300

    init commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostNews::Application.config.secret_key_base = 'e1e79723d05920f02e18d9f90f318731214cd05c36764bf926c4bf31ff7aa4e81975a326a96d47e2988e2459f5e3a3a11e82733f5ac5739ff1b576ad1b063393'
