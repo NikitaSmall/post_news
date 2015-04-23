06837e233cb6281c0d245b1def395acfabba5171
nikitasmall <nikitasosnov@yahoo.com>
2015-04-23 23:20:15 +0300

    basic functions for Post model; test (yes, TDD!)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,32 @@
 class Post < ActiveRecord::Base
   acts_as_taggable
   belongs_to :user
+  scope :main, -> { where(main: true) }
+  scope :hidden, -> { where(main: false) }
+  scope :by_position, -> { order(position: :desc) }
+  scope :by_position_asc, -> { order(position: :asc) }
+
+  def next
+    Post.where('position > ?', position).order(position: :asc).first
+  end
+
+  def prev
+    Post.where('position < ?', position).order(position: :asc).last
+  end
+
+  def switch(post)
+    self.position, post.position = post.position, self.position
+    post.save
+    self.save
+  end
+
+  def main!
+    self.main = true
+    self.save
+  end
+
+  def hide!
+    self.main = false
+    self.save
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "should_show_next_post" do
+    post_one = posts(:one)
+    post_two = posts(:two)
+
+    next_post = post_one.next
+    assert_equal post_two, next_post
+  end
+
+  test "should_show_prev_post" do
+    post_one = posts(:one)
+    post_two = posts(:two)
+
+    prev_post = post_two.prev
+    assert_equal post_one, prev_post
+  end
+
+  test "should_switch_positions" do
+    post_one = posts(:one)
+    post_two = posts(:two)
+    old_post_one_pos = post_one.position
+    old_post_two_pos = post_two.position
+
+    post_one.switch post_two
+
+    assert_equal post_one.position, old_post_two_pos
+    assert_equal post_two.position, old_post_one_pos
+  end
+
+  test "should_force_post_to_show_on_main_page" do
+    post = posts(:one)
+
+    assert_difference('Post.main.count', 1) do
+      post.main!
+    end
+    assert post.main
+  end
+
+  test "should_hide_post" do
+    post = posts(:three)
+
+    assert_difference('Post.hidden.count', 1) do
+      post.hide!
+    end
+    assert !post.main
   end
 end
